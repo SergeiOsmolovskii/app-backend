d4c6ccc6232d8c2ba76a819c1088574061ce9215
Osmolovskii Sergei <osmolovskii_sergei@mail.ru>
2022-08-25 21:55:06 +0300

    feat: init swagger settings

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { dirname, join } from 'path';
+import { parse } from 'yaml';
+import { readFile } from 'fs/promises';
 import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const rootDirname = dirname(__dirname);
+  const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
+  const document = parse(DOC_API);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(process.env.PORT || 4000);
 }
 
