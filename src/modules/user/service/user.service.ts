1909d355af212a8b45a77a6add0492a770dab210
Osmolovskii Sergei <osmolovskii_sergei@mail.ru>
2022-08-26 23:10:04 +0300

    feat: add user entity and dto

--- src/modules/user/service/user.service.ts
+++ src/modules/user/service/user.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserEntity } from '../entity/user.entity';
+
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly usersRepository: Repository<UserEntity>
+  ) {}
+
+  public async getAllUsers() {
+    return (await this.usersRepository.find()).map(user => user.toResponse());
+  }
+
+  
+
+}
